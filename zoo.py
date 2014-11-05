f1460e750c5b46a02dc9e9685eb532daf8160e4e
modzozo <modzozo@abv.bg>
2014-11-05 21:40:02 +0200

    add reproduce method to zoo

--- zoo.py
+++ zoo.py
@@ -5,6 +5,8 @@ class Zoo:
     ANIMAL_INCOME = 60
     MEAT_1KG_PRICE = 4
     GRASS_1KG_PRICE = 2
+    PREGNANCY_PERIOD = 6
+
 
     def __init__(self, capacity, budget):
         self.animals = []
@@ -23,7 +25,9 @@ class Zoo:
 
 
     def get_income(self):
-        self.budget += 60 * len(self.animals)
+        daily_income = 60 * len(self.animals)
+        return daily_income
+
 
     def get_outcome(self):
         outcome = 0
@@ -37,7 +41,7 @@ class Zoo:
         return self.budget
 
     def dead_animals(self):
-        list_dead_aniamals = []
+        list_dead_animals = []
         for animal in self.animals:
             if animal.die():
                 self.animals.remove(animal)
@@ -51,21 +55,33 @@ class Zoo:
         if random_number > 0.5:
             return "male"
         return "female"
-        #TODO
+
+
+    def get_female_animals(self):
+        female_animals = []
+        for animal in self.animals:
+            if animal.gender == "female":
+                female_animals.append(animal)
+        return female_animals
+
+
+    def reproduce(self,animal1, animal2):
+
+        if animal1.species == animal2.species and animal1.gender != animal2.gender:
+            if animal1 in self.get_female_animals() and animal1.non_pregnant_period >= Zoo.PREGNANCY_PERIOD:
+                animal1.pregnant_period = 0
+                animal1.non_pregnant_period = None
 
     def newborn_baby(self):
-        pass
+        babies = []
+        for animal in self.get_female_animals():
+            if animal.pregnant_period == animal.json_species_data['gestation_period']:
+                animal.pregnant_period = None
+                animal.non_pregnant_period = 0
 
-    def reproduce(self):
-        for animal in self.animals:
-            if animal.gender="female" and animal1.gender != animal2.gender:
-            if animal1.gender == "female" and animal1.age - animal1.last_pregnancy >= 6:
-                animal1.last_pregnancy = animal1.age
-            elif animal2.age - animal2.last_pregnancy >= 6:
-                animal2.last_pregnancy = animal2.age
-            baby = Animal(animal1.species, 0, animal1.set_name(), self.gender_baby(), None)
-            self.animals.append(baby)
-        else:
-            message = "Cannot reproduce. Try again later"
-            raise ValueError(message)
+                baby = Animal(animal.species, 0, animal.set_name(), self.gender_baby(), None)
+                babies.append(baby)
+                self.animals.append(baby)
+
+        return babies
 
