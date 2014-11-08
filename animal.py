72e3e13b8491535fd9f236b54ce51b556aa4270c
Valentina Zhekova <valentina.p.zhekova@gmail.com>
2014-11-08 20:51:58 +0200

    added data members for pregnancy

--- animal.py
+++ animal.py
@@ -1,4 +1,6 @@
 import json
+from random import uniform
+#from zoo import Zoo
 
 
 class Animal:
@@ -6,7 +8,12 @@ class Animal:
     # "species": ['name 1', 'name 2', ...]
     SPECIES_NAMES = {}
 
-    def __init__(self, species, age_in_months, name, gender, kilos_weight):
+    PREGNANCY_PERIOD = 6
+    # PREGNANCY_PERIOD = Zoo.PREGNANCY_PERIOD
+    DAYS_APPROXIMATION = 30
+
+    def __init__(self, species, age_in_months, name,
+                 gender, kilos_weight=None):
         self.json_species_data = self.__set_json_species_data(
             "database.json", species)
 
@@ -16,7 +23,7 @@ class Animal:
         self.gender = self.__set_gender(gender)
         self.kilos_weight = self.__set_kilos_weight(kilos_weight)
 
-        self.non_pregnant_period = None
+        self.non_pregnant_period = Animal.PREGNANCY_PERIOD
         self.pregnant_period = None
 
     def __set_species(self, species):
@@ -61,38 +68,52 @@ class Animal:
             return json.loads(content)[species]
         return None
 
-    # depending on simulated time interval, increase
-    # animal's weight and age; weight limit is average_weight
     def grow(self, month_time):
         self.age_in_months += month_time
-        self.weight_passed_time(month_time)
-        self.pregnancy_passed_time(month_time)
+        self.__weight_simulated_time(month_time)
+        self.__pregnancy_simulated_time(month_time)
 
-    def pregnancy_passed_time(self, month_time):
-        if self.species == 'female':
-            if self.non_pregnant_period is not None:
-                self.pregnant_period += month_time
-            else:
-                self.non_pregnant_period += month_time
-
-    def weight_passed_time(self, month_time):
+    def __weight_simulated_time(self, month_time):
         average_weight = self.json_species_data['average_weight']
         if self.kilos_weight < average_weight:
-            c = self.json_species_data['weight_age_ratio'] * self.age_in_months
-            self.kilos_weight = min(c, average_weight)
+            weight_age_ratio = self.json_species_data['weight_age_ratio']
+            weight_increased = weight_age_ratio * self.age_in_months
+            self.kilos_weight = min(weight_increased, average_weight)
 
-    # return how many kilos the animal should consume
+    # not accurate for simulated time long enough for more than one pregnancy
+    # but in such case babies check problem would appear...
+    def __pregnancy_simulated_time(self, month_time):
+        if self.gender == 'female':
+            if self.pregnant_period is not None:
+                pregnant_period_increased = self.pregnant_period + month_time
+                gestation_limit = self.json_species_data['gestation_period']
+                self.pregnant_period = min(pregnant_period_increased,
+                                           gestation_limit)
+            else:
+                non_pregnant_increased = self.non_pregnant_period + month_time
+                non_pregnancy_limit = Animal.PREGNANCY_PERIOD
+                self.non_pregnant_period = min(non_pregnant_increased,
+                                               non_pregnancy_limit)
+
     def eat(self):
         return self.json_species_data['food_weight_ratio'] * self.kilos_weight
 
-    # *** not really --> ranomize it...
-    # if chance of dying is above 1, the animal dies
     def die(self):
-        # self.age is in months so roughly turn it to days
-        chance_of_dying = self.age_in_months * 30 // self.json_species_data['life_expectancy']
-        if chance_of_dying >= 1:
+        age_in_days = self.age_in_months * Animal.DAYS_APPROXIMATION
+        life_expectancy = self.json_species_data['life_expectancy']
+        chance_of_dying = age_in_days / life_expectancy
+
+        death_arbiter = uniform(0, 2)
+
+        if death_arbiter < chance_of_dying:
             self.SPECIES_NAMES[self.species].remove(self.name)
-        return chance_of_dying >= 1
+            return True
+        else:
+            return False
+
+    def set_name(self):
+        get_name = input("\nHow to name the baby?: ")
+        return get_name
 
     def __str__(self):
         return "{}: {}, {} months, {} kilos".format(self.name,
