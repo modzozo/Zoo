8ce698b2effc19b7c2cb4f72c02be077fcf16eed
Valentina Zhekova <valentina.p.zhekova@gmail.com>
2014-11-08 20:53:23 +0200

    tests for class Animal

--- animal_test.py
+++ animal_test.py
@@ -4,11 +4,6 @@ from animal import Animal
 
 class AnimalTests(unittest.TestCase):
 
-#  дава грешка, понеже за всеки тест създава такава чайка, а името е вече заето
-#  т.е. всеки тест трябва да си прави лично животно?
-#    def setUp(self):
-#        self.animal = Animal("seagull", 3, "Stamat", 'male', 1.250)
-
     def test__init(self):
         self.animal = Animal("bear", 14, "Poll 1", 'male', 10.0)
         self.assertEqual("bear", self.animal.species)
@@ -58,16 +53,14 @@ class AnimalTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.animal._Animal__set_gender("tomato")
 
-    def test_set_weight_passed_argument(self):
+    def test_set_kilos_weight_passed_argument(self):
         self.animal = Animal("bear", 14, "Poll 8", 'male', 10.0)
         self.assertEqual(42, self.animal._Animal__set_kilos_weight(42))
 
-    def test_set_weight_no_passed_argument_take_newborn_weight(self):
+    def test_set_kilos_weight_no_passed_argument_take_newborn_weight(self):
         self.animal = Animal("bear", 14, "Poll 9", 'male', 2)
         self.assertEqual(self.animal.json_species_data['newborn_weight'],
                          self.animal._Animal__set_kilos_weight(None))
-
-# __set_json_data_for_species which are in database is already tested
 
     def test_set_json_species_data_species_not_in_database(self):
         self.animal = Animal("bear", 14, "Poll 10", 'male', 10.0)
@@ -75,34 +68,44 @@ class AnimalTests(unittest.TestCase):
             "database.json", "seagull"))
 
     def test_grow(self):
-        self.animal = Animal("bear", 3, "Poll 11", 'male', 10.0)
-        self.animal.grow(3)
-        self.assertEqual(54, self.animal.kilos_weight)
-        self.assertEqual(6, self.animal.age_in_months)
-        # missing tests for pregnancy and the new functions
-        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        self.animal_male = Animal("bear", 3, "Poll 11", 'male', 10.0)
+        self.animal_male.grow(3)
+        self.assertEqual(54, self.animal_male.kilos_weight)
+        self.assertEqual(6, self.animal_male.age_in_months)
 
-    def test_grow_average_weight_reached(self):
+    def test_weight_simulated_time_average_weight_reached(self):
         self.animal = Animal("bear", 3, "Poll 12", 'male', 10.0)
         self.animal.grow(10)
         self.assertEqual(100, self.animal.kilos_weight)
+
+    def test_pregnancy_simulated_time_pregnant_female(self):
+        self.animal_female = Animal("bear", 13, "Pollinka 1", 'female', 10.0)
+        self.animal_female.pregnant_period = 4
+        self.animal_female.non_pregnant_period = None
+        self.animal_female.grow(3)
+        self.assertEqual(6, self.animal_female.pregnant_period)
+        self.assertEqual(None, self.animal_female.non_pregnant_period)
+
+    def test_pregnancy_simulated_time_non_pregnant_female(self):
+        self.animal_female = Animal("bear", 13, "Pollinka 2", 'female', 10.0)
+        self.animal_female.non_pregnant_period = 7
+        self.animal_female.grow(3)
+        self.assertEqual(6, self.animal_female.non_pregnant_period)
+        self.assertEqual(None, self.animal_female.pregnant_period)
 
     def test_eat(self):
         self.animal = Animal("bear", 3, "Poll 13", 'male', 10.0)
         self.assertEqual(2, self.animal.eat())
 
-    def test_die_still_alive(self):
-        self.animal = Animal("bear", 3, "Poll 14", 'male', 10.0)
+    def test_die(self):
+        # I don't know how to test this function
+        # because it interacts with the global dictionary of names in Animal
+        # and that brings some limitations
+        pass
 
-        self.animal.age = 0
-        self.assertFalse(self.animal.die())
-
-    def test_die_true(self):
-        self.animal = Animal("bear", 3, "Poll 15", 'male', 10.0)
-
-        self.animal.age = 1
-        self.assertTrue(self.animal.die())
-        self.assertTrue(self.animal.name not in Animal.SPECIES_NAMES)
+    def test_set_name_for_the_baby(self):
+        # also not sure how to test such things
+        pass
 
     def test_str(self):
         self.animal = Animal("bear", 3, "Poll 16", 'male', 10.0)
